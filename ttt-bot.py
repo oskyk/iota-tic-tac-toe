5867b61a8d118c7433938d8dfe2274c600ac6110
Oskar Hladky <oskar.hladky@bitpanda.com>
2018-05-17 16:54:27 +0200

    exception caching

--- ttt-bot.py
+++ ttt-bot.py
@@ -8,11 +8,14 @@ iota_client = IotaClient()
 prev_matches = set()
 
 while True:
-    matches = set(iota_client.get_open_matches())
-    intersection = matches.intersection(prev_matches)
-    for match in intersection:
-        thread = Thread(target=TttAI, kwargs={'iota_client': iota_client, 'addr_index': int(match)})
-        thread.start()
-        print('Match id: {} started!'.format(match))
-    prev_matches = matches
+    try:
+        matches = set(iota_client.get_open_matches())
+        intersection = matches.intersection(prev_matches)
+        for match in intersection:
+            thread = Thread(target=TttAI, kwargs={'iota_client': iota_client, 'addr_index': int(match)})
+            thread.start()
+            print('Match id: {} started!'.format(match))
+        prev_matches = matches
+    except Exception as error:
+        print(str(error))
     sleep(20)
